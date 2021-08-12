41ae97c0ea60b2f7a315eb859a64cefe3be149ae
samilo-del <camilovelenciapadilla@gmail.com>
2021-08-12 20:47:29 +0000

    add point 6

--- models/state.py
+++ models/state.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
     cities = relationship("City", cascade="all,delete", backref="state")
+
     @property
     def cities(self):
         from models import storage
