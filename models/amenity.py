47b77e9ab4f26dac65d9208dd75dc10ccb668ae6
samilo-del <camilovelenciapadilla@gmail.com>
2021-08-15 00:55:12 +0000

    add point

--- models/amenity.py
+++ models/amenity.py
@@ -7,7 +7,9 @@ from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
     """Amenity class definition"""
-    __tablename__ = "amenities"
+    __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        place_amenities = relationship('Place',
+                                       secondary='place_amenity',
+                                       back_populates='amenities')
