160f78c334d40b19e6fdee4e7672051eca95ec2a
samilo-del <camilovelenciapadilla@gmail.com>
2021-08-26 23:12:36 +0000

    add point

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" function do_pack. """
+
+from datetime import datetime
+import os
+from fabric.api import local
+
+
+def do_pack():
+    """ ... """
+    try:
+        form = datetime.now().strftime("%Y%m%d%H%M%S")
+        if not os.path.exists("versions"):
+            os.mkdir("versions")
+
+        f1le = "versions/web_static_{}.tgz".format(forma)
+        local("tar -cvzf {} web_static".format(file_))
+        return f1le
+    except:
+        return(None)
